d5adcfd4762c86d0cb6b8319f77dbd64c63e8d76
Ankit Agarwal <37203631+ankitpro@users.noreply.github.com>
2020-05-23 21:29:27 +0200

    Added model_design function

--- model_automation.py
+++ model_automation.py
@@ -122,3 +122,49 @@ def LSTM_complete_auto(data, train_percent, optimizer_name, loss_name, units, mu
                         'Hidden Layer': hidden_layer_count, 'Epochs': Epochs, 'Batch Size': Batch_Size,'Exection Time': exec_time, 'Actual and Prediction': predicted_normalized}
   print('Duration: {}'.format(exec_time))
   return model_information
+
+
+def Model_Design(optimizer_name, loss_name, model, units, \
+                 multiple_units, activation_function, input_shape_row, \
+                 input_shape_col, dropout_unit, hidden_layer_count):
+  """
+  optimizer_name = Name of the optimizer you want to use in Sequential model. (optimiazer_name = 'adam')
+  
+  loss_name = Name of the loss you want to use in Sequential model. (loss_name ='mean_squared_error')
+  
+  model = Variable name given to which the model is assigned. (model = Sequential())
+  
+  units = Dimension of the cell state. (units = 40)
+  
+  multiple_units = Number of units added to the consecutive layers. (multiple_units=20 [units+20 for each consecutive layer])
+  
+  activation_function = Name of the activation function used. (activation_function = 'relu')
+  
+  input_shape_row = Row dimension of your hidden state. (input_shape_row = 1)
+  
+  input_shape_col = Column dimension of your hidden state. (input_shape_col = 3)
+  
+  dropout_unit = No of units defined in dropout which varies from 0.2 - 0.8. (dropout_unit = 0.2)
+  
+  hidden_layer_count = No of hidden layers that you want. (hidden_layer_count = 3)
+  """
+  from keras.models import Sequential
+  from keras.layers import Dense,LSTM,Dropout
+  model.add(LSTM(units = units, activation = activation_function, return_sequences = True, input_shape = (input_shape_row, input_shape_col)))
+  model.add(Dropout(dropout_unit))
+
+  #units = units + multiple_units
+  for i in range(0, hidden_layer_count):
+    model.add(LSTM(units = units, activation = activation_function, return_sequences = True))
+    model.add(Dropout(dropout_unit))
+    #units = units + multiple_units
+
+  model.add(LSTM(units = units, activation = activation_function))
+  model.add(Dropout(dropout_unit))
+
+  model.add(Dense(units = 1))
+  model.compile(optimizer=optimizer_name, loss = loss_name)
+  
+  #model.summary()
+  
+  return model
