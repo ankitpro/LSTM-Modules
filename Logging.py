6a71ad3fcc4bf61b6f0f51eff8425e42d3eeb8fa
Ankit Agarwal <37203631+ankitpro@users.noreply.github.com>
2020-05-24 17:06:25 +0200

    Update Logging.py

--- Logging.py
+++ Logging.py
@@ -1,8 +1,9 @@
-def Logging_Notebook(log_name):
+def Logging_Notebook(notebook_name):
 
   #importing module 
   import logging, logging.handlers
   import time
+  from datetime import datetime
   from pytz import timezone, utc
 
   def customTime(*args):
@@ -11,30 +12,16 @@ def Logging_Notebook(log_name):
       converted = utc_dt.astimezone(my_tz)
       return converted.timetuple()
 
+  logger = logging.getLogger('spam_application')
   logging.Formatter.converter = customTime
-  logger.error("customTime")
+  #logger.error("customTime")
 
   ##Creating and Configuring logger
   fhandler = logging.FileHandler(filename=notebook_name, mode='a')
   formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
   fhandler.setFormatter(formatter)
-  #logging.Formatter.converter = time.gmtime
 
   #Setting the threshold of logger to DEBUG 
   logger.addHandler(fhandler)
   logger.setLevel(logging.DEBUG)
   return logger
-
-
-"""
-Usage Example:
-notebook_name = "Logging For the Code1.log"
-logger_info = Logging_Notebook(notebook_name)
-#Test messages 
-logger_info.info("\n---------- Logging Started ----------") 
-logger_info.debug("Harmless debug Message") 
-logger_info.info("Just an information") 
-logger_info.warning("Its a Warning") 
-logger_info.error("Did you try to divide by zero") 
-logger_info.critical("This is critical error") 
-"""
