778bea26ccfafe0741c536287cb2df1a4476908b
Ankit Agarwal <37203631+ankitpro@users.noreply.github.com>
2022-07-01 11:08:27 +0200

    replaced f1_score
    replaced: f1 = f1_score(y_test.round(), y_pred_classes.round())
    with: f1 = f1_score(y_test.round(), y_pred_classes.round(), average='micro')

--- model_automation.py
+++ model_automation.py
@@ -165,7 +165,7 @@ def LSTM_complete_auto(data, train_percent, optimizer_name, loss_name, units, mu
   recall = recall_score(y_test.round(), y_pred_classes.round(), pos_label='positive', average='micro')
   print('Recall: %f' % recall)
   # f1: 2 tp / (2 tp + fp + fn)
-  f1 = f1_score(y_test.round(), y_pred_classes.round())
+  f1 = f1_score(y_test.round(), y_pred_classes.round(), average='micro')
   print('F1 score: %f' % f1)
   # kappa
   kappa = cohen_kappa_score(y_test.round(), y_pred_classes.round())
